33494c1476235835c3ef4a124237746bfd2fe630
Frederik Omlor <freddy-omlor@web.de>
2024-10-06 01:33:45 +0200

    Trying to fix inaccurate type sizes on UNIX

--- src/core/types/types.h
+++ src/core/types/types.h
@@ -21,12 +21,12 @@ namespace Borealis::Types
 
 		// @TODO: Adapt for Linux
 		typedef int_fast8_t		Int8;
-		typedef int_fast16_t	Int16;
+		typedef short			Int16;
 		typedef int_fast32_t	Int32;
 		typedef int_fast64_t	Int64;
 
 		typedef uint_fast8_t	Uint8;
-		typedef uint_fast16_t	Uint16;
+		typedef unsigned short	Uint16;
 		typedef uint_fast32_t	Uint32;
 		typedef uint_fast64_t	Uint64;
 
