ace9971c90ce3059445ac7abe1d8df3ee5f530ea
Frederik Omlor <freddyomlor@gmail.com>
2025-03-16 21:26:35 +0100

    Trying to fix implementation for Liunx

--- src/config.h
+++ src/config.h
@@ -1,5 +1,13 @@
 #ifdef BOREALIS_BUILD_DLL
-#define BOREALIS_API __declspec(dllexport)
+	#if defined(_WIN32) || defined(_WIN64)
+		#define BOREALIS_API __declspec(dllexport)
+	#else	
+		#define BOREALIS_API __attribute__((visibility("default")))
+	#endif
 #else
-#define BOREALIS_API __declspec(dllimport)
+	#if defined(_WIN32) || defined(_WIN64)
+		#define BOREALIS_API __declspec(dllimport)
+	#else	
+		#define BOREALIS_API __attribute__((visibility("default")))
+	#endif
 #endif // BOREALIS_BUILD_DLL
