3e46e607e48a8550e81c774564b67c4644168850
Frederik Omlor <63716222+FreddyOm@users.noreply.github.com>
2025-03-18 16:01:58 +0100

    Adapted tests

--- tests/test_debug.cpp
+++ tests/test_debug.cpp
@@ -9,9 +9,40 @@ TEST(DebugTest, Log)
 {
 	EXPECT_GT(Log("Test Log"), 0);
 	EXPECT_GT(Log("Test message"), 0);
+	EXPECT_GT(Log(""), 0);
 	EXPECT_GT(Log("LoGGING Some 8320384 more!!"), 0);
 	
 	
 	EXPECT_GT(Log("Test %s", "Log"), 0);
 	EXPECT_GT(Log("1 + 1 = %u", 2), 0);
+}
+
+
+TEST(DebugTest, Warning)
+{
+	EXPECT_GT(LogWarning("Test Warning"), 0);
+	EXPECT_GT(LogWarning(""), 0);
+	EXPECT_GT(LogWarning("LoGGING Some test more!!"), 0);
+
+
+	EXPECT_GT(LogWarning("Test %s", "Warning"), 0);
+	EXPECT_GT(LogWarning("1 + 1 = %u", 2), 0);
+}
+
+TEST(DebugTest, Error)
+{
+	EXPECT_GT(LogError("Test Warning"), 0);
+	EXPECT_GT(LogError(""), 0);
+	EXPECT_GT(LogError("LoGGING Some test more!!"), 0);
+
+
+	EXPECT_GT(LogError("Test %s", "Warning"), 0);
+	EXPECT_GT(LogError("1 + 1 = %u", 2), 0);
+}
+
+TEST(DebugTest, Assert)
+{
+	EXPECT_EQ(Assert(0 == 0, "Test Warning"), 0);
+	EXPECT_GT(Assert(1 == 2, "1 is not equal to 2"), 0);
+	EXPECT_EQ(Assert(true || false, "LoGGING Some test more!!"), 0);
 }
