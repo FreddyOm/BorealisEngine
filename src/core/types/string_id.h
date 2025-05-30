9b64ada8df7ec746d5fd7f8de9b0edc1f064c6b9
Frederik Omlor <63716222+FreddyOm@users.noreply.github.com>
2025-05-30 18:15:34 +0200

    Fixed issue with including <cstring> in release config.

--- src/core/types/string_id.h
+++ src/core/types/string_id.h
@@ -3,16 +3,17 @@
 #include "types.h"
 #include "../debug/logger.h"
 
-#include "../math/crc_hash.h"
 #if defined(BOREALIS_RELEASE) || defined(BOREALIS_MINSIZEREL)
+#include "../math/crc_hash.h"
+#include <cstring>
 #endif
 
 namespace Borealis::Types
 {
-	typedef uint64 StringId;
+	typedef uint64Ptr StringId;
 
 #if defined(BOREALIS_DEBUG) || defined(BOREALIS_RELWITHDEBINFO)
-	BOREALIS_API extern Types::uint64Ptr InternString(const char* str);
+	BOREALIS_API extern StringId InternString(const char* str);
 
 	/// <summary>
 	/// Evaluates to a hash value and stores the native string internally.
