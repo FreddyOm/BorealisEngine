1b2d6b2f708da94e4c4685dbc5967f635cf76e78
Frederik Omlor <63716222+FreddyOm@users.noreply.github.com>
2026-05-07 15:33:30 +0200

    Minor fix for warnings referring to unused return values in file_io.cpp

--- src/core/io/file_io.cpp
+++ src/core/io/file_io.cpp
@@ -1,5 +1,6 @@
 #include "file_io.h"
 #include "../debug/logger.h"
+#include <stdio.h>
 
 using namespace Borealis::Types;
 using namespace Borealis::Memory;
@@ -12,7 +13,7 @@ namespace Borealis::IO
 
 		Assert(f != NULL, "Couldn't open file located at path \"%s\".", filePath);
 
-		fseek(f, 0, SEEK_END);
+		Assert(fseek(f, 0, SEEK_END), "Error setting stream pointer to filestream!");
 		uint64 file_size = (uint64)ftell(f);
 		
 		Assert(file_size == -1, "Couldn't evaluate the size of file located at path \"%s\".", filePath);
@@ -22,10 +23,14 @@ namespace Borealis::IO
 		{
 			MemAllocJanitor janitor(allocCntxt);
 			
-			fseek(f, 0, SEEK_SET);
+			Assert(fseek(f, 0, SEEK_SET), "Error setting stream pointer to filestream!");
 			file_data = RefCntAutoPtr<char>::AllocBlock(file_size);
-			fread(file_data.RawPtr(), 1, file_size, f);
-			fclose(f);
+			Types::uint64 bytes_read = fread(file_data.RawPtr(), 1, file_size, f);
+
+			if (bytes_read < file_size)
+				LogError("The number of read bytes was less than the number of bytes present in the file! Missing %u bytes!", file_size - bytes_read);
+
+			Assert(fclose(f) == 0, "Error closing the file!");
 
 			Assert(file_data.RawPtr() != nullptr, "Couldn't allocate memory for reading the file at path \"%s\".", filePath);
 			out_dataSize = file_size;
